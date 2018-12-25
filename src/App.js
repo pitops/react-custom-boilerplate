07b61e2e91c7df80e2239432b4377b0ff40530dc
Petros Kyriakou <9pitops@gmail.com>
2018-12-25 15:54:04 +0200

    added prettier, eslint, webpack merge

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { hot } from 'react-hot-loader'
+
+class app extends Component {
+  state = {
+    count: 0
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello world!!!!</h1>
+      </div>
+    )
+  }
+}
+
+export default hot(module)(app)
