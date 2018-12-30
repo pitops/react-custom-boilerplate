38da8dd49bda9f90f2834697c80de50a0ad76fef
Petros Kyriakou <9pitops@gmail.com>
2018-12-30 22:41:30 +0200

    updated jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  setupTestFrameworkScriptFile: '<rootDir>/testSetup.js'
+  // explicit declaration that the testing environment is intended for browsers
+  testEnvironment: 'jest-environment-jsdom',
+  setupTestFrameworkScriptFile: '<rootDir>/testSetup.js',
+  moduleNameMapper: {
+    '\\.module\\.css$': 'identity-obj-proxy',
+    '\\.(css|sass|scss|less)$': require.resolve('./test/style-mock.js')
+  }
 }
